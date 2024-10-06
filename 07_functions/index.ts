59d714eea714203720e2476f07be3cf3d5244924
shahriaarrr <shahriaarrr@gmail.com>
2024-10-06 09:05:10 +0330

    work with functions

---
+++ 07_functions/index.ts
@@ -0,0 +1,9 @@
+function calculateTax(income: number, taxYear: number = 2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+
+    return income * 1.3;
+}
+
+calculateTax(10_000, 2022);
+calculateTax(20_000);
