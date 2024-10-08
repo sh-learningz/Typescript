0b3562feedd5110d9b0566fda94ad532385c1f79
shahriaarrr <shahriaarrr@gmail.com>
2024-10-08 17:46:45 +0330

    create class

---
+++ Section_4/01_Create_Class.ts
@@ -0,0 +1,19 @@
+class Account {
+    id: number;
+    owner: string;
+    balance: number;
+
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    deposit(amount: number): void {
+        if (amount <= 0) {
+            throw new Error("invalid amount");
+        }
+
+        this.balance += amount;
+    }
+}
