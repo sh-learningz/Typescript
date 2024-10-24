e71a19f9a557cc4ec89b08fa11291455550f882d
shahriaarrr <shahriaarrr@gmail.com>
2024-10-24 19:47:32 +0330

    Decorator Composition

--- Section_6/02_Parameterized_Decorators.ts
+++ Section_6/02_Parameterized_Decorators.ts
@@ -15,7 +15,14 @@ function Component(options: componentOptions) {
     }
 }
 
-@Component
+// like f(g(x))
+function Pipe(constructor: Function) {
+    console.log('Pipe decorator call');
+    constructor.prototype.pipe = true;
+}
+
+@Component({selector: '#my-profile'})
+@Pipe
 class ProfileCommponent {
 
 }
