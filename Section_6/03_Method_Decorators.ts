01eb12aaaa63e5aa43337e7aecc74f95db39350e
shahriaarrr <shahriaarrr@gmail.com>
2024-10-25 18:08:28 +0330

    Method Decorators

---
+++ Section_6/03_Method_Decorators.ts
@@ -0,0 +1,16 @@
+function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const original = descriptor.value as Function;
+    descriptor.value = (message: string) => {
+        console.log('Before');
+        original.call(this, message);
+        console.log('After');
+    }
+}
+
+class Person {
+    @Log
+    say(message: string) {
+        console.log('Person says: ', message);
+        
+    }
+}
